3c1aa471432c5fa76ad5b4c2c8dd934a6deae6aa
lain <2836969767@qq.com>
2023-04-19 10:34:13 +0800

    course half

--- service/course.py
+++ service/course.py
@@ -1,1 +1,32 @@
+import requests
 
+import Config
+from service.user import User
+
+
+class course:
+    record_id : str
+    course_name : str
+    course_description : str
+    teacher_name : str
+    status : int
+    def __init__(self):
+        pass
+def getCourseList(u : User):
+    ret = requests.post(f"{Config.BASE_URL}/api/v1/records/list",
+                        data=u.token).json()
+    if ret["code"] == 200:
+        course_list = []
+        for i in ret["data"]["list"]:
+            t = course()
+            t.record_id = i["_id"]
+            t.course_name = i["course"]["course_name"]
+            t.course_description = i["course"]["description"]
+            t.teacher_name = i["teacher"]["username"]
+            t.status = int(i["status"])
+            course_list.append(t)
+        # u.loginStatus = True
+        # u.expire = ret["data"]["expire"] // 1000
+        return course_list
+    else:
+        raise Exception("网络请求出错")
--- view/course.py
+++ view/course.py
@@ -2,12 +2,13 @@ import sys
 
 import webview
 
-from service.user import User, userLogin, saveUser, getLoginStatus
-import service.user
+from service import user
+import service.user,service.course
 from utils.logger import logger
 import view.monitor
 
 courseWindow: webview.Window
+
 
 class JsAPI:
     def quit(self):
@@ -16,24 +17,30 @@ class JsAPI:
         if courseWindow:
             courseWindow.destroy()
         sys.exit()
+
     def getCourseList(self):
-        logger.info("Load Course List From HTTP")
-        #ret = getLoginStatus()
-        #here need request
+        logger.info("Load Course List From HTTP Port")
+        ret = service.course.getCourseList(user.USER)
         if not ret:
             return None
-        return {
-            'username': ret.username,
-            'password': ret.password,
-            'login': ret.loginStatus
-        }
-def getCourseList():
+        return ret.json()
 
+
+def quitSystem():
+    logger.info("Close Button Pressed, System Closing...")
+    if view.login.loginWindow:
+        view.login.loginWindow.destroy()
+    sys.exit()
+
+
+def getCourseList():
     global courseWindow
     courseWindow = webview.create_window("课程列表",
-                                        "templates/login.html",
-                                        width=440,
-                                        height=440,
-                                        resizable=False,
-                                        js_api=JsAPI())
+                                         "templates/login.html",
+                                         width=440,
+                                         height=440,
+                                         resizable=False,
+                                         js_api=JsAPI())
+    courseWindow.events.closed += quitSystem
+
     return courseWindow
